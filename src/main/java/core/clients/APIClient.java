7aeb0c845d44734eb4ff6d1554efb97948e15ee2
maxkanzas <maxkanz1991@gmail.com>
2024-12-18 21:39:00 +0400

    change APIClient.java, build.gradle

--- src/main/java/core/clients/APIClient.java
+++ src/main/java/core/clients/APIClient.java
@@ -1,10 +1,8 @@
 package core.clients;
-
 import core.settings.ApiEndpoints;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -16,8 +14,7 @@ public class APIClient {
         this.baseUrl = determineBaseUrl();
     }
     private String determineBaseUrl() {
-        String environment = System.getenv("ENV"); // Или используйте
-        System.getProperty("env", "test");
+        String environment = System.getProperty("env", "test");
         String configFileName = "application-" + environment + ".properties";
         Properties properties = new Properties();
         try (InputStream input = getClass().getClassLoader().getResourceAsStream(configFileName)) {
@@ -28,6 +25,43 @@ public class APIClient {
         } catch (IOException e) {
             throw new IllegalStateException("Unable to load configuration file:" + configFileName, e);
         }
-        return properties.getProperty("baseUrl");
+        return properties.getProperty("baseUrl"); // извлекает значение по ключу "baseUrl" из объекта Properties.
+    }
+    // Настройка базовых параметров HTTP-запросов
+    private RequestSpecification getRequestSpec(){
+        return RestAssured.given()
+                .baseUri(baseUrl)
+                .header("Content-type", "application/json")
+                .header("Accept", "application/json");
+    }
+    // GET-запрос на эндпоинт /ping
+    public Response ping(){
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.PING.getPath())
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+    }
+    // GET-запрос на эндпоинт /booking
+    public Response getBooking(){
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKING.getPath())
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+    }
+    // GET-запрос на эндпоинт /booking
+    public Response getBookingId(){
+        return getRequestSpec()
+                .when()
+                .get(ApiEndpoints.BOOKINGID.getPath())
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
     }
 }
