fd396d5593b69d7d6fdca48162a83d55a385c38e
maxkanzas <maxkanz1991@gmail.com>
2024-12-30 18:14:17 +0400

    add BookinResponse and UpdateBookingRequest

---
+++ src/main/java/core/models/UpdateBookingRequest.java
@@ -0,0 +1,72 @@
+package core.models;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UpdateBookingRequest {
+    @JsonProperty("firstname")
+    private String firstname;
+
+    @JsonProperty("lastname")
+    private String lastName;
+
+    @JsonProperty("totalprice")
+    private int totalPrice;
+
+    @JsonProperty("depositpaid")
+    private boolean depositPaid;
+
+    @JsonProperty("bookingdates")
+    private BookingDates bookingdates;
+
+    @JsonProperty("additionalneeds")
+    private String additionalNeeds;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public boolean isDepositPaid() {
+        return depositPaid;
+    }
+
+    public void setDepositPaid(boolean depositPaid) {
+        this.depositPaid = depositPaid;
+    }
+
+    public String getAdditionalNeeds() {
+        return additionalNeeds;
+    }
+
+    public void setAdditionalNeeds(String additionalNeeds) {
+        this.additionalNeeds = additionalNeeds;
+    }
+
+    public BookingDates getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(BookingDates bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+}
